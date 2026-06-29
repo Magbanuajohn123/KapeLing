baa1af3533f487c981ce22c742298dfc07406e55
scilearn <scilearnthesis@gmail.com>
2026-06-29 20:01:09 +0800

    Remove nixpacks config

--- resources/views/userRent.blade.php
+++ resources/views/userRent.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Menu List</title>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link
